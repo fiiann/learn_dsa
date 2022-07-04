9b0ba03e9000a3d7903f9b08c203c551e5a2decd
Garfianto Dwi <garfianto@ptdes.net>
2022-07-04 20:01:27 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args){
+        Leetcode.mainTwoSum();
+//        int reverse = Leetcode.reverseDigits(1234);
+//        System.out.print(reverse);
+//        Pattern.mainPattern(5);
+//        Pattern.pattern31(5);
+    }
+
+}
